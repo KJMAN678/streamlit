804dc919ef5cd551cadaf7402c4be3610bfdb587
KJMAN <koji.smz@gmail.com>
2020-12-19 21:50:29 +0900

    3rd commit

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit
 
-echo "\
-[general]\n\
-email = \"koji.smz@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
